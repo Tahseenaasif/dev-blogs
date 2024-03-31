3f12b120198fd2527ec25841fcb290fe4347a067
tahseen <tahseenaasif@gmail.com>
2024-03-31 12:10:50 +0530

    footer final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignIn from './pages/SignIn'; 
 import Header from './components/Header';
-
+import Footer from './components/footer'
 export default function App() {
   return (
     <BrowserRouter>
@@ -20,6 +20,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
